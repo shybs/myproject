963b2921f5a1dcb45957c8454e76fbae6965381a
zhangjun_leader@126.com <zhangjun_leader@126.com>
2017-07-03 10:27:01 +0800

    新建项目

---
+++ test/src/com/test/mytest/MyTest.java
@@ -0,0 +1,13 @@
+package com.test.mytest;
+
+/**
+ * 类描述：测试  
+ * 创建人：ZJ
+ * 创建时间：2017年6月30日 下午6:07:37
+ * @version V1.0
+ */
+public class MyTest {
+  public static void main(String[] args) {
+    System.out.println("测试");
+  }
+}
