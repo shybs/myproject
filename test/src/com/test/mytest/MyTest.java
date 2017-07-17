4faf8d35164955c24259a1e6d9d00681384de5e9
yixian_zhang@163.com <yixian_zhang@163.com>
2017-07-17 17:02:08 +0800

    测试

--- test/src/com/test/mytest/MyTest.java
+++ test/src/com/test/mytest/MyTest.java
@@ -7,7 +7,7 @@ package com.test.mytest;
  * @version V1.0
  */
 public class MyTest {
-  public static void main(String[] args) {
-    System.out.println("测试Git");
-  }
+	public static void main(String[] args) {
+		System.out.println("我的第一个Git测试");
+	}
 }
